bf0daa66a3279dc1f1863347452869bb8d01c338
Junshu Chen <locpk123@gmail.com>
2014-10-30 14:36:54 -0400

    Finished lab 2-3 and 2-4

--- lab2/Q3.cpp
+++ lab2/Q3.cpp
@@ -1,2 +1,59 @@
 #include "lab2.h"
-using namespace std;
+
+
+int Q3()
+{
+	using std::cout;
+	using std::cin;
+	char cAnw1, cAnw2, cAnw3;
+	int iScore = 0;
+	cout << "True or False Exam - Enter T or F\n\n";
+	cout << "Question 1: \'elseif\' is a keyword in C++: ";
+	cin >> cAnw1;
+	if (cAnw1 == 'F' || cAnw1 == 'f')
+	{
+		++iScore;
+		cout << "CORRECT!Your score is " << iScore << " !\n";
+	}
+	else if (cAnw1 == 'T' || cAnw1 == 't')
+	{
+		cout << "Incorrect!\n";
+	}
+	else
+	{
+		cout << "ERROR! You did not input T or F!\n";
+	}
+
+	cout << "\'rand()%26+25\' will produce a number within 25 -> 26: ";
+	cin >> cAnw2;
+	if (cAnw2 == 'T' || cAnw2 == 't')
+	{
+		++iScore;
+		cout << "CORRECT!Your score is " << iScore << " !\n";
+	}
+	else if (cAnw2 == 'F' || cAnw2 == 'f')
+	{
+		cout << "Incorrect!\n";
+	}
+	else
+		cout << "ERROR! You did not input T or F!\n";
+
+	cout << "A switch's \'default\' handles any unmatched values: ";
+	cin >> cAnw3;
+	if (cAnw3 == 'T' || cAnw3 == 't')
+	{
+		++iScore;
+		cout << "CORRECT!Your score is " << 1 << " !\n";
+	}
+	else if (cAnw3 == 'F' || cAnw3 == 'f')
+	{
+		cout << "Incorrect!\n";
+	}
+	else
+	{
+		cout << "ERROR! You did not input T or F!\n"; 
+	}
+	cout << "Final score is : " << iScore;
+
+		return EXIT_SUCCESS;
+}
--- lab2/Q4.cpp
+++ lab2/Q4.cpp
@@ -1,2 +1,15 @@
 #include "lab2.h"
-using namespace std;
+
+int Q4()
+{
+	using std::cout;
+	using std::cin;
+	int iMultiple = 0;
+	cout << "Please enter a multiple of 5: ";
+	cin >> iMultiple;
+	if (iMultiple % 5 == 0)
+		cout << "This is correct!\n";
+	else
+		cout << "This is not correct!\n";
+	return EXIT_SUCCESS;
+}
